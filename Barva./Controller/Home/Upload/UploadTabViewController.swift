87e92142cb7b90052d44cc17844a5211cdf70528
Donggaem <ehdrua465111@naver.com>
2022-09-19 22:50:18 +0900

    .

--- Barva./Controller/Home/Upload/UploadTabViewController.swift
+++ Barva./Controller/Home/Upload/UploadTabViewController.swift
@@ -27,7 +27,6 @@ class UploadTabViewController: UIViewController, PHPickerViewControllerDelegate 
     private let maxCount = 99
     
     var images: [UIImage] = []
-    var imageData: [Data] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -102,27 +101,29 @@ class UploadTabViewController: UIViewController, PHPickerViewControllerDelegate 
     }
 
     
-    //MARK: POST IMG
-    func postIMG(images: [UIImage]) {
-        let headers: HTTPHeaders = ["Content-type": "multipart/form-data"]
-        AF.upload(multipartFormData: { (multipartFormData) in
-            
-        }, to: BarvaURL.imgURL, method: .post, headers: headers).responseJSON(completionHandler: { (response) in    //헤더와 응답 처리
-                        print(response)
-                        
-                        if let err = response.error{    //응답 에러
-                            print(err)
-                            return
-                        }
-                        print("success")        //응답 성공
-                        
-                        let json = response.data
-                        
-                        if (json != nil){
-                            print(json)
-                        }
-                    })
-    }
+//    //MARK: POST IMG
+//    func postIMG(images: [UIImage]) {
+//        let headers: HTTPHeaders = ["Content-type": "multipart/form-data"]
+//        AF.upload(multipartFormData: { (multipartFormData) in
+//
+//        }, to: BarvaURL.imgURL, method: .post, headers: headers).responseJSON(completionHandler: { (response) in    //헤더와 응답 처리
+//                        print(response)
+//
+//                        if let err = response.error{    //응답 에러
+//                            print(err)
+//                            return
+//                        }
+//                        print("success")        //응답 성공
+//
+//                        let json = response.data
+//
+//                        if (json != nil){
+//                            print(json)
+//                        }
+//                    })
+//    }
+    
+    
 }
 
 //MARK: Extension FSPagerView
