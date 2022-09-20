c74a832f1e6c89d5e703aab163aa7f4be5e61f8b
Donggaem <ehdrua465111@naver.com>
2022-09-20 21:35:47 +0900

    UI조정

--- Barva./Controller/Login/Find/IDFindTabViewController.swift
+++ Barva./Controller/Login/Find/IDFindTabViewController.swift
@@ -25,29 +25,28 @@ class IDFindTabViewController: UIViewController {
     private func setUI() {
         
         //버튼 모서리
-        emailBtn.layer.cornerRadius = 15
-        checkNumBtn.layer.cornerRadius = 15
         idFindBtn.layer.cornerRadius = 5
         
-        //버튼 활성/비활성 액션
-        self.emailTextField.addAction(UIAction(handler: { _ in
-            if self.emailTextField.text?.isEmpty == true {
-                self.emailBtn.isEnabled = false
-                self.emailBtn.backgroundColor = UIColor(red: 0.941, green: 0.941, blue: 0.941, alpha: 1)
-            } else {
-                self.emailBtn.isEnabled = true
-                self.emailBtn.backgroundColor = .black
-            }
-        }), for: .editingChanged)
+//        //버튼 활성/비활성 액션
+//        self.emailTextField.addAction(UIAction(handler: { _ in
+//            if self.emailTextField.text?.isEmpty == true {
+//                self.emailBtn.isEnabled = false
+//                self.emailBtn.backgroundColor = UIColor(red: 0.941, green: 0.941, blue: 0.941, alpha: 1)
+//            } else {
+//                self.emailBtn.isEnabled = true
+//                self.emailBtn.backgroundColor = .black
+//            }
+//        }), for: .editingChanged)
+//
+//        self.checkNumTextField.addAction(UIAction(handler: { _ in
+//            if self.checkNumTextField.text?.isEmpty == true {
+//                self.checkNumBtn.isEnabled = false
+//                self.checkNumBtn.backgroundColor = UIColor(red: 0.941, green: 0.941, blue: 0.941, alpha: 1)
+//            } else {
+//                self.checkNumBtn.isEnabled = true
+//                self.checkNumBtn.backgroundColor = .black
+//            }
+//        }), for: .editingChanged)
         
-        self.checkNumTextField.addAction(UIAction(handler: { _ in
-            if self.checkNumTextField.text?.isEmpty == true {
-                self.checkNumBtn.isEnabled = false
-                self.checkNumBtn.backgroundColor = UIColor(red: 0.941, green: 0.941, blue: 0.941, alpha: 1)
-            } else {
-                self.checkNumBtn.isEnabled = true
-                self.checkNumBtn.backgroundColor = .black
-            }
-        }), for: .editingChanged)
     }
 }
